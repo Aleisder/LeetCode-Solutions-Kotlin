67bf903f137288b8aee7694d106590beed1296ab
Aleisder <tsarenko_d@inbox.ru>
2022-09-02 19:32:57 +0300

    Task 704

---
+++ src/main/kotlin/leetcode/704. Binary Search.kt
@@ -0,0 +1,29 @@
+package leetcode
+
+fun search(nums: IntArray, target: Int): Int {
+    var low = 0
+    var high = nums.size - 1
+
+    while (low <= high) {
+        val indexOfMid = (low + high) / 2
+        val guess = nums[indexOfMid]
+
+        if (guess == target) {
+            return indexOfMid
+        }
+
+        else if (guess < target) {
+            low = indexOfMid + 1
+        }
+
+        else {
+            high = indexOfMid - 1
+        }
+    }
+    return -1
+}
+
+fun main() {
+    println(search(nums = intArrayOf(-1, 0, 3, 5, 9, 12), target = 9)) //4
+    println(search(nums = intArrayOf(-1, 0, 3, 5, 9, 12), target = 2)) //-1
+}
